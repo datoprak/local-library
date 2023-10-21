787a0b99f9d391890db25a0847876c4136636f49
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-21 09:23:12 +0300

    Add lifespan virtual property to author model

--- models/author.js
+++ models/author.js
@@ -21,16 +21,20 @@ authorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
 
-authorSchema.virtual("date_of_birth_formatted").get(function () {
-  return this.date_of_birth
-    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
-    : "";
-});
-
-authorSchema.virtual("date_of_death_formatted").get(function () {
-  return this.date_of_death
-    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
-    : "";
+authorSchema.virtual("lifespan").get(function () {
+  let lifespan = "";
+  if (this.date_of_birth && this.date_of_death) {
+    lifespan = `(${DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    )} - ${DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    )})`;
+  } else if (this.date_of_birth) {
+    lifespan = `(${DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    )})`
+  }
+  return lifespan;
 });
 
 module.exports = mongoose.model("Author", authorSchema);
