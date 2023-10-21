9eb4c093289a7dc73de694b0ce6f6b801cf3d915
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-21 06:53:09 +0300

    Format bookinstance due date with luxon and add conditional rendering to booklist and bookinstancelist

--- models/bookInstance.js
+++ models/bookInstance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -16,6 +17,12 @@ const bookInstanceSchema = new Schema({
 
 bookInstanceSchema.virtual("url").get(function () {
   return `/catalog/bookinstance/${this._id}`;
+});
+
+bookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(
+    DateTime.DATETIME_MED
+  );
 });
 
 module.exports = mongoose.model("BookInstance", bookInstanceSchema);
