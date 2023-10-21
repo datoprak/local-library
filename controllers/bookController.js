12531546c1f57e8f221b97a2bd5edd16f93ebd8d
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-21 05:42:32 +0300

    Create index controller and view it

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,8 +1,32 @@
 const asyncHandler = require("express-async-handler");
+const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookInstance");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  // res.send("NOT IMPLEMENTED: Site Home Page");
-  res.render("index", { title: "library" });
+  const [
+    bookCount,
+    bookInstanceCount,
+    availableBookInstanceCount,
+    authorCount,
+    genreCount,
+  ] = await Promise.all([
+    Book.countDocuments({}).exec(),
+    BookInstance.countDocuments({}).exec(),
+    BookInstance.countDocuments({ status: "Available" }).exec(),
+    Author.countDocuments({}).exec(),
+    Genre.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Local Library Home",
+    bookCount,
+    bookInstanceCount,
+    availableBookInstanceCount,
+    authorCount,
+    genreCount,
+  });
 });
 
 exports.bookList = asyncHandler(async (req, res, next) => {
