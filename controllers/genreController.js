10cc6707caeb5c1cf753265cc0e4826e3c04739f
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-22 09:01:17 +0300

    Create genreForm controllers and view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Genre = require("../models/genre");
 const Book = require("../models/book");
 const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
 
 exports.genreList = asyncHandler(async (req, res, next) => {
   const allGenres = await Genre.find().sort({ name: 1 }).exec();
@@ -9,33 +10,53 @@ exports.genreList = asyncHandler(async (req, res, next) => {
 });
 
 exports.genreDetail = asyncHandler(async (req, res, next) => {
-  if (!mongoose.isValidObjectId(req.params.id)) {
-    const err = new Error("Genre not found.");
-    err.status = 404;
-    return next(err);
-  }
-  
+  const err = new Error("Genre not found.");
+  err.status = 404;
+
+  if (!mongoose.isValidObjectId(req.params.id)) return next(err);
+
   const [genre, booksInGenre] = await Promise.all([
     Genre.findById(req.params.id).exec(),
     Book.find({ genre: req.params.id }, "title summary").exec(),
   ]);
 
-  if (!genre) {
-    const err = new Error("Genre not found.");
-    err.status = 404;
-    return next(err);
-  }
+  if (!genre) return next(err);
 
   res.render("genreDetail", { title: "Genre Detail", genre, booksInGenre });
 });
 
-exports.genreCreateGet = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create GET");
-});
+exports.genreCreateGet = (req, res, next) => {
+  const genre = { name: "" };
+  res.render("genreForm", { title: "Create Genre", errors: null, genre });
+};
 
-exports.genreCreatePost = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create POST");
-});
+exports.genreCreatePost = [
+  body("name", "Genre name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const genre = new Genre({ name: req.body.name });
+    if (!errors.isEmpty()) {
+      res.render("genreForm", {
+        title: "Create Genre",
+        genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const genreExist = await Genre.findOne({ name: req.body.name })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+      if (genreExist) res.redirect(genreExist.url);
+      else {
+        await genre.save();
+        res.redirect(genre.url);
+      }
+    }
+  }),
+];
 
 exports.genreDeleteGet = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Genre delete GET");
